5bda7266a018e4d045ce687015c5da37ccc30b21
Paul-Jules Micolet <p.micolet@gmail.com>
2012-02-10 23:57:00 +0000

    Added code folder

---
+++ code/src/com/codestorm/TowerDefenceActivity.java
@@ -0,0 +1,13 @@
+package com.codestorm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TowerDefenceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
